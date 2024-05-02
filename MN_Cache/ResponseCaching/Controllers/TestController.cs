a8671718b532c08b81e859cc7f8e0d9add5795bf
kolotko111 <milosznowicki96@wp.pl>
2024-05-02 11:13:04 +0200

    response caching

---
+++ MN_Cache/ResponseCaching/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResponseCaching.Controllers;
+
+[Route("api/test")]
+[ApiController]
+[ResponseCache(
+    Duration = 60, 
+    Location = ResponseCacheLocation.Any
+)]
+public class TestController : ControllerBase
+{
+    private static List<int> _randomintList;
+    public TestController()
+    {
+        if (_randomintList is null)
+        {
+            _randomintList = new List<int>();
+            Random random = new Random(15);
+            foreach (var enumerator in Enumerable.Range(0, 50))
+            {
+                _randomintList.Add(random.Next(1, 100));
+            }
+        }
+    }
+    
+    [HttpPost]
+    public ActionResult<List<int>> Post()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public ActionResult<List<int>> Get()
+    {
+        return Ok(_randomintList);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<int> GetOne(int id)
+    {
+        return Ok(_randomintList[id]);
+    }
+}
